965007be5096ddf72ab6496cde1203984cf3601c
Xja689 <numaairoldi31@gmail.com>
2024-11-13 17:54:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from dotenv import load_dotenv
 import discord
 from keep_alive import keep_alive
 
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Playing Solo";
+    discordPresence.details = "Competitive";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageKey = "tuvaallerenenfertoi";
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 5;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
 
 client = discord.Client(intents=discord.Intents.all())
 
